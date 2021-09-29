74a22ccb55f2423ca668e8c663988b251b8541e6
markhsumark <tulsa0415@gmail.com>
2021-09-29 11:12:51 +0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by 徐易中 on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
